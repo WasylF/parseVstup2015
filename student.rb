dc92e8792bee40a453529d98b96fe41ee8b70eb7
Vasyl Franchuk <WasylFranchuk@gmail.com>
2016-06-24 17:06:16 +0300

    add average_zno method

--- student.rb
+++ student.rb
@@ -11,4 +11,12 @@ class Student
   def to_s
     "name: #{@name} \t priority: #{@priority} \t zno: #{@zno}"
   end
+
+  def average_zno
+    values = @zno.split(' ')
+    if values.length == 0
+      values << 0.0
+    end
+    values.inject(:+).to_f / values.size.to_f
+  end
 end
